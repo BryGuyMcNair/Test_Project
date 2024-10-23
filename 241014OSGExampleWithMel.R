4ef8619ad2b2740c70779274a26b61637f6912e1
Bryan McNair <Bryan.McNair@cuanschutz.edu>
2024-10-23 15:41:45 -0600

    Second try

---
+++ 241014OSGExampleWithMel.R
@@ -0,0 +1,26 @@
+
+
+#########################################
+# Example script to use with Mels help
+# 10-14-2024
+# Bryan McNair MS
+#########################################
+
+# Temp change 2
+
+
+dat<-matrix(1:30,nrow=10,ncol=3)
+
+summaryOut<-summary(dat)
+
+summaryOut
+
+dat2<-t(dat)
+
+summaryOut2<-summary(dat2)
+
+summaryOut2
+
+
+
+
